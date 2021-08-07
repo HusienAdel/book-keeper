d1ec68d59ac6a2533a90d808e854e49341cf5cd6
Husien adel <husien@gmail.com>
2021-08-07 14:38:56 +0300

    minor update js

--- script.js
+++ script.js
@@ -12,9 +12,6 @@ const websiteUrlEl = document.querySelector("#website-url");
 // bookmar container
 const bookmarksContainer = document.querySelector("#bookmarks-container");
 
-
-
-
 let bookmarks = [];
 
 modalShow.addEventListener("click", showModal);
@@ -29,12 +26,11 @@ modalClose.addEventListener("click", () => {
 });
 
 window.addEventListener("click", (e) => {
-    e.target.classList.contains("modal-container") ?
-        modal.classList.remove("show-modal") :
-        false;
+    e.target.classList.contains("modal-container")
+        ? modal.classList.remove("show-modal")
+        : false;
 });
 
-bookmarkForm.addEventListener("submit", storeBookmark);
 
 function validate(siteName, siteUrl) {
     const expression =
@@ -54,112 +50,107 @@ function validate(siteName, siteUrl) {
     return true;
 }
 
-
-
 function fetchBookmarks() {
+    if (localStorage.getItem('bookmarks') !=="[]") {
 
-    if (localStorage.getItem('bookmarks')) {
-        bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+    } else  {
 
-    } else {
-        bookmarks = [{
+        bookmarks = [
+            {
+                name: "egydes",
+                url: "https://www.egydes.com",
+            },
+        ];
+        localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
 
-
-            name: 'egydes',
-            url: 'https://www.egydes.com',
-        }, ];
-        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     }
     buildBookmarks();
-
-
 }
-
 
 function storeBookmark(e) {
     e.preventDefault();
 
-    const sitename = websiteNameEl.value;
+    let sitename = websiteNameEl.value;
     let siteUrl = websiteUrlEl.value;
 
     if (!siteUrl.includes("http://") && !siteUrl.includes("https://")) {
         siteUrl = `https://${siteUrl}`;
     }
     if (!validate(sitename, siteUrl)) {
-
         return false;
     }
 
     const bookmark = {
         name: sitename,
-        url: siteUrl
+        url: siteUrl,
     };
 
     bookmarks.push(bookmark);
+    console.log(bookmark);
 
-    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
     fetchBookmarks();
-    ShowBookmarks();
+    buildBookmarks();
     bookmarkForm.reset();
 }
 
-fetchBookmarks();
-
 
 function buildBookmarks() {
+    bookmarksContainer.textContent = '';
+
     bookmarks.forEach((bookmark) => {
+        const item = document.createElement("div");
+        item.classList.add("item");
+        const deleteButton = document.createElement("i");
+
+        deleteButton.className = "far fa-times-circle";
+
+        deleteButton.setAttribute("id", "delete");
 
 
 
-        const item = document.createElement('div');
-        item.classList.add('item');
-        const deleteButton = document.createElement('i');
+        deleteButton.setAttribute('onclick', `deleteBookmark('${bookmark.url}')`);
 
-        deleteButton.className = 'far fa-times-circle';
 
-        deleteButton.setAttribute('id', 'delete');
-        const name = document.createElement('div');
-        name.classList.add('name');
-        const img = document.createElement('img');
-        img.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${bookmark.url}`);
+        const name = document.createElement("div");
+        name.classList.add("name");
+        const img = document.createElement("img");
+        img.setAttribute(
+            "src",
+            `https://s2.googleusercontent.com/s2/favicons?domain=${bookmark.url}`
+        );
 
-        const link = document.createElement('a');
-        link.setAttribute('href', `${bookmark.url}`);
-        link.setAttribute('target', '_blank');
+        const link = document.createElement("a");
+        link.setAttribute("href", `${bookmark.url}`);
+        link.setAttribute("target", "_blank");
         link.textContent = bookmark.name;
 
-
-        name.appendChild(img);
-        name.appendChild(link);
-        item.appendChild(name);
-        item.appendChild(deleteButton);
+        name.append(img);
+        name.append(link);
+        item.append(name);
+        item.append(deleteButton);
 
         bookmarksContainer.appendChild(item);
-
-
-
-
-
-
-
-
-
-    })
-}
-
-const deleteButtons = document.querySelectorAll('#delete');
-
-deleteButtons.forEach((delbt) => {
-
-
-
-    delbt.addEventListener('click', (e) => {
-        let key = e.target.parentElement.querySelector('a').textContent;
-        let value = e.target.parentElement.querySelector('a').getAttribute('href');
-        localStorage.removeItem('bookmarks', JSON.stringify({ name: key, url: value }));
-
-        e.target.parentElement.remove();
-
     });
 
-})
+
+
+}
+
+
+function deleteBookmark(myUrl) {
+    bookmarks.forEach((bookmark, i) => {
+        if (bookmark.url === myUrl) {
+            bookmarks.splice(i, 1);
+        }
+    });
+
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+    fetchBookmarks();
+}
+
+bookmarkForm.addEventListener("submit", storeBookmark);
+
+
+fetchBookmarks();
